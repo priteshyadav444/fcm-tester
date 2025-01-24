ced8a7c4e72d76aa147477f59eb511533e3974e0
priteshyadav444 <priteshyadav2015@Gmail.com>
2025-01-25 02:33:12 +0530

    design updated

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const functions = require("firebase-functions");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files (CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Route to serve the HTML file
+app.get("/home", (req, res) => {
+  console.log("asdasd");
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.get("/test", (req, res) => {
+  res.send("The /test route is working!");
+});
+
+// Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+
+if (process.env.APP_ENV == "local") {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+if (process.env.APP_ENV == "production") {
+  exports.api = functions.https.onRequest(app);
+}
